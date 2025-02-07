3551ee598b43bfd89cbcd513cec3ed49555cb9be
Hanson-HSChang <hanson.hschang@gmail.com>
2025-02-06 19:07:04 -0600

    wip: add evaluate_model_iteration_skip and load_model(...)

--- tests/learning/test_learning.py
+++ tests/learning/test_learning.py
@@ -54,8 +54,10 @@ class TestBaseLearningModule:
     def test_model_load(
         self, simple_model: SimpleModel, tmp_path: Path
     ) -> None:
-        simple_model.save(tmp_path / "model.pt")
-        loaded_model = SimpleModel.load(tmp_path / "model.pt")
+        simple_model.save(tmp_path / "model.pt", trained_epochs=10)
+        loaded_model, loaded_model_info = SimpleModel.load(
+            tmp_path / "model.pt"
+        )
         assert isinstance(loaded_model, SimpleModel)
         assert loaded_model._config == simple_model._config
         for (
@@ -70,3 +72,4 @@ class TestBaseLearningModule:
         ):
             assert simple_model_key == loaded_model_key
             assert torch.equal(simple_model_value, loaded_model_value)
+        assert loaded_model_info["trained_epochs"] == 10
